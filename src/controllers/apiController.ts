d9d39a43c09d71c1034ef306ba4eb496afbeeb98
renatopaivabv <renatopaivabv@gmail.com>
2022-11-24 19:40:40 -0300

    add informação da url da documentação na index

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
 
 export const index = (req: Request, res: Response) =>
-  res.json({ index: "Api de frases" });
+  res.json({
+    index: "Api de frases",
+    docs: "Acesse /api-docs para ver a documentação",
+  });
